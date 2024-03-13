1e55af505f69e5410c01dfeca54fdbc2f632e7dd
jcisking <gocats098@gmail.com>
2024-03-12 20:52:02 -0700

    modified HeroesScreen.kt

--- app/src/main/java/com/example/superheroapp2/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroapp2/HeroesScreen.kt
@@ -9,12 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -29,17 +32,32 @@ fun HeroListItem(
 ) {
     Card(
         elevation = CardDefaults.cardElevation(),
+        shape = RoundedCornerShape(16.dp),
         modifier = modifier
+    ) {
+        Row(modifier = Modifier
             .padding(16.dp)
             .height(72.dp)
-    ) {
-        Row(modifier = Modifier) {
+        ) {
             Column {
-                Text(text = stringResource(id = R.string.hero1))
-                Text(text = stringResource(id = R.string.description1))
+                Text(
+                    text = stringResource(id = R.string.hero1),
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = stringResource(id = R.string.description1),
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
             Box() {
-                Image(painter = painterResource(id = R.drawable.android_superhero1), contentDescription = null)
+                Image(
+                    painter = painterResource(id = R.drawable.android_superhero1),
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .size(72.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentDescription = null
+                )
             }
         }
     }
